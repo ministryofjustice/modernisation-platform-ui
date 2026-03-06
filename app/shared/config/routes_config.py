edf06d21cd23aebc6a7f840ead89c3b920eb1f49
Rich Green <richard.green@digital.justice.gov.uk>
2026-03-06 15:35:30 +0000

    Remove repository standards and acronyms

--- app/shared/config/routes_config.py
+++ app/shared/config/routes_config.py
@@ -1,11 +1,5 @@
 from flask import Flask
 
-from app.projects.repository_standards.routes.api import repository_standards_api
-from app.projects.repository_standards.routes.deprecated import (
-    repository_standards_deprecated,
-)
-from app.projects.repository_standards.routes.main import repository_standards_main
-from app.projects.acronyms.routes.main import acronyms_main
 from app.shared.routes.auth import auth_route
 from app.shared.routes.main import main
 from app.shared.routes.robots import robot_route
@@ -16,18 +10,6 @@ def configure_routes(app: Flask) -> None:
     app.register_blueprint(auth_route, url_prefix="/auth")
     app.register_blueprint(main)
     app.register_blueprint(robot_route)
-
-    app.register_blueprint(
-        repository_standards_main, url_prefix="/repository-standards/"
-    )
-    app.register_blueprint(
-        repository_standards_api, url_prefix="/repository-standards/api"
-    )
-    app.register_blueprint(repository_standards_deprecated, url_prefix="/")
-
-    app.register_blueprint(
-        acronyms_main, url_prefix="/acronyms/"
-    )
 
     app.register_blueprint(
         modernisation_platform_main, url_prefix="/modernisation-platform/"
